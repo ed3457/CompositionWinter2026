48f79b89e7d5ee032b00aabb33389fa65cdc34a6
Kamel R <kamel.r@wayne.edu>
2026-09-21 18:09:15 -0400

    finished impimintation of the address class

--- Address.cpp
+++ Address.cpp
@@ -1,1 +1,55 @@
 #include "Address.h"
+#include <iostream>
+using namespace std;
+void Address::printAddress()
+{
+	cout << getStreetName() << endl;
+	cout << getCity() << endl;
+	cout << getZipCode() << endl;
+
+}
+
+void Address::setStreetName(string streetName)
+{
+	this->streetName = streetName;
+
+}
+
+string Address::getStreetName()
+{
+	return streetName;
+}
+
+void Address::setZipCode(string zipCode)
+{
+	this->zipcode = zipCode;
+}
+
+string Address::getZipCode()
+{
+	return zipcode;
+}
+
+void Address::setCity(string city)
+{
+	this->city = city; 
+}
+
+string Address::getCity()
+{
+	return city;
+}
+
+Address::Address()
+{
+	setStreetName("Not set yet");
+	setZipCode("Not set yet");
+	setCity("Not set yet");
+}
+
+Address::Address(string streetName, string zipCode, string city)
+{
+	setStreetName(streetName);
+	setZipCode(zipCode);
+	setCity(city);
+}
--- Address.h
+++ Address.h
@@ -1,5 +1,31 @@
 #pragma once
+#include <string>
+//#include <regex>
+using namespace std; 
 class Address
 {
+private: 
+	string streetName;
+	string zipcode;
+	string city; 
+
+public:
+
+	void printAddress();
+
+	void setStreetName(string streetName);
+	string getStreetName();
+
+	void setZipCode(string zc);
+	string getZipCode();
+
+	void setCity(string city);
+	string getCity();
+
+	Address();
+
+	Address(string streetName, string zipCode, string city);
+
+
 };
 
